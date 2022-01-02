3abe8be018899e8f307c35c6af7e5d9edb1d4c14
tniemeye19 <timothy.niemeyer19@gmail.com>
2022-01-02 16:10:29 -0500

    created styled 'cards' for portfolio items that transition on hover

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,19 +1,23 @@
 import React from "react";
-import { FaGithub } from 'react-icons/fa';
+import { FaGithub, FaRocket } from 'react-icons/fa';
 import projects from './projects.json';
 import './style.css';
 
 function Project() {
 
     return (
-        <div className="projects">
+        <div className="projects row justify-content-center">
             {
                 projects && projects.length > 0 && projects.map((project) =>
-                <div className="project">
+                <div className="project col-4">
                     <img src={require('../../assets/images/' + project.image + '.png')} alt={project.title} />
                     <div className="project-info">
-                        <h3><a href={project.appLink}>{project.title}</a></h3>
-                        <i><a href={project.githubLink}><FaGithub /></a></i>
+                        <h3>{project.title}</h3>
+                        <p>{project.description}</p>
+                        <div className="icons">
+                            <i><a href={project.githubLink}><FaGithub /></a></i>
+                            <i><a href={project.appLink}><FaRocket /></a></i>
+                        </div>
                     </div>
                 </div>)
             }
