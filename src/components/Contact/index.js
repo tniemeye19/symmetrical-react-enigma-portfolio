afdc86c0db6d1f5100c24b9f2d535d3314106d4f
tniemeye19 <timothy.niemeyer19@gmail.com>
2022-01-02 19:21:06 -0500

    adjust resume stylesheet, add react-animations through npm i

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -35,29 +35,39 @@ function Contact() {
     };
 
     return (
-        <section className="contact">
-            <h2>Contact Me</h2>
-            <form className="contact-form" onSubmit={handleSubmit}>
-                <div className='form-box'>
-                    <label htmlFor='name'>Name:</label>
-                    <input type='text' name='name' defaultValue={name} onBlur={handleChange} />
-                </div>
-                <div className='form-box'>
-                    <label htmlFor='email'>Email address:</label>
-                    <input type='text' name='email' defaultValue={email} onBlur={handleChange} />
-                </div>
-                <div className='form-box'>
-                    <label htmlFor='message'>Message:</label>
-                    <textarea name='name' rows='5' defaultValue={message} onBlur={handleChange} />
-                </div>
-                {errorMessage && (
-                    <div>
-                        <p className="error-text">{errorMessage}</p>
+        <div className="contact-container d-flex justify-content-center row">
+            <h2 className='text-center'>Contact Me</h2>
+            <section className="contact col-4">
+                <form className="contact-form" onSubmit={handleSubmit}>
+                    <div className='form-box'>
+                        <label htmlFor='name'>Name:</label>
+                        <input type='text' name='name' defaultValue={name} onBlur={handleChange} />
                     </div>
-                )}
-                <button type="submit">Submit</button>
-            </form>
-        </section>
+                    <div className='form-box'>
+                        <label htmlFor='email'>Email address:</label>
+                        <input type='text' name='email' defaultValue={email} onBlur={handleChange} />
+                    </div>
+                    <div className='form-box'>
+                        <label htmlFor='message'>Message:</label>
+                        <textarea name='name' rows='5' defaultValue={message} onBlur={handleChange} />
+                    </div>
+                    {errorMessage && (
+                        <div>
+                            <p className="error-text">{errorMessage}</p>
+                        </div>
+                    )}
+                    <button className="btn btn-success" type="submit">Send Message</button>
+                </form>
+            </section>
+            <section className='info col-4'>
+                <h3>If you would like to talk about a project or simply say hello, I'd love to hear from you!
+                    <br></br>
+                    All you need to do is fill out this form and send me an email.
+                    <br></br>
+                    Thanks!
+                </h3>
+            </section>
+        </div>
     )
 }
 
